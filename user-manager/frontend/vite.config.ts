e51992135481f481936e89878e145ae3c03775ca
rasshivalov <nikita_rasshivalov@mail.ru>
2025-07-28 19:49:44 +0300

    update frontend architecture

--- user-manager/frontend/vite.config.ts
+++ user-manager/frontend/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  build: {
+    outDir: "build",
+  },
   server: {
     port: 3000,
     proxy: {
