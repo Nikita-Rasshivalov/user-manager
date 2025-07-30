57fdb3c06eebe65c46dcc2feb517ee9cc8fdc112
rasshivalov <nikita_rasshivalov@mail.ru>
2025-07-30 18:45:58 +0300

    fix vite cfg

--- user-manager/frontend/vite.config.ts
+++ user-manager/frontend/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   build: {
@@ -22,5 +21,9 @@ export default defineConfig({
         secure: false,
       },
     },
+  },
+  preview: {
+    port: 3100,
+    host: true,
   },
 });
